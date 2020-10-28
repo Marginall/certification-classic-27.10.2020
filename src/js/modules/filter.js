911d323126a2e6a233c263c54193969478c0e9de
Marginall <mistmee2016@gmail.com>
2020-10-28 19:37:26 +0200

    20 00 build

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -5,7 +5,7 @@ export const filter = () => {
 	const $controls = $(document).find('input, select');
 	const currentPage = $filterForm.data('page');
 
-	let data = {
+	const data = {
 		params: {
 			brand: [],
 			manufacturer: '',
@@ -21,53 +21,59 @@ export const filter = () => {
 	};
 
 	const createGetParams = (data) => {
-		let url = new URL(window.location.href);
+		const url = new URL(window.location.href);
 		let tempData = {};
 
 		if (data.params && data.pagination) {
 			tempData = Object.assign(tempData, data.params, data.pagination);
-			console.log(tempData['year']);
-			
-			url.searchParams.set('page', tempData['page']);
+			console.log(tempData.year);
 
-			if (tempData['year'] !== '') {
-				url.searchParams.set('year', tempData['year']);
+			url.searchParams.set('page', tempData.page);
+
+			if (tempData.year !== '') {
+				url.searchParams.set('year', tempData.year);
 			}
 
-			url.searchParams.set('price', tempData['price'][0] + ',' + tempData['price'][1]);
+			url.searchParams.set('price', tempData.price[0] + ',' + tempData.price[1]);
 
-			if (tempData['model'] !== '') {
-				url.searchParams.set('model', tempData['model']);
+			if (tempData.model !== '') {
+				url.searchParams.set('model', tempData.model);
 			}
 
-			if (tempData['manufacturer'] !== '') {
-				url.searchParams.set('manufacturer', tempData['manufacturer']);
+			if (tempData.manufacturer !== '') {
+				url.searchParams.set('manufacturer', tempData.manufacturer);
 			}
 
-			if (tempData['brand'] !== '') {
-				url.searchParams.set('brand', tempData['brand']);
+			if (tempData.brand !== '') {
+				url.searchParams.set('brand', tempData.brand);
 			}
 
-			url.searchParams.set('sort', tempData['sort']);
-			url.searchParams.set('per-page', tempData['perPage']);
+			url.searchParams.set('sort', tempData.sort);
+			url.searchParams.set('per-page', tempData.perPage);
 		}
 
 		window.history.pushState(null, null, url.href);
 	};
 
 	$controls.on('change', () => {
-		let brands = [];
+		const brands = [];
 
 		$controls.each((i, el) => {
 			const $el = $(el);
-			let brand = $el.prop('checked') ? $el.parent().find('span').text() : null;
-			let manufacturer = $el.attr('name') === 'marka' ? $el.find('option:selected').text() : null;
-			let model = $el.attr('name') === 'model' ? $el.find('option:selected').text() : null;
-			let year = $el.attr('name') === 'year' ? parseInt($el.val()) : null;
-			let minPrice = parseInt($('#price-from').val());
-			let maxPrice = parseInt($('#price-to').val());
-			let sort = $el.attr('name') === 'sort' ? $el.find('option:selected').text() : null;
-			let perPage = $el.attr('name') === 'per_page' ? $el.find('option:selected').text() : null;
+			const brand = $el.prop('checked') ? $el.parent().find('span').text() : null;
+			const manufacturer =
+				$el.attr('name') === 'marka' ? $el.find('option:selected').text() : null;
+			const model =
+				$el.attr('name') === 'model' ? $el.find('option:selected').text() : null;
+			const year = $el.attr('name') === 'year' ? parseInt($el.val()) : null;
+			const minPrice = parseInt($('#price-from').val());
+			const maxPrice = parseInt($('#price-to').val());
+			const sort =
+				$el.attr('name') === 'sort' ? $el.find('option:selected').text() : null;
+			const perPage =
+				$el.attr('name') === 'per_page'
+					? $el.find('option:selected').text()
+					: null;
 
 			if (brand) {
 				brands.push(brand);
@@ -81,28 +87,27 @@ export const filter = () => {
 			if (model && !isNaN($el.val())) {
 				data.params.model = model;
 			}
-	
+
 			if (year) {
 				data.params.year = year;
 			}
-	
-			if(minPrice && maxPrice) {
+
+			if (minPrice && maxPrice) {
 				data.params.price = [];
 				data.params.price.push(minPrice, maxPrice);
 			}
-	
+
 			if (sort) {
 				data.pagination.sort = sort;
 			}
-	
+
 			if (perPage) {
 				data.pagination.perPage = perPage;
 			}
-	
+
 			if (currentPage !== '') {
 				data.pagination.page = currentPage;
 			}
-	
 		});
 
 		createGetParams(data);
